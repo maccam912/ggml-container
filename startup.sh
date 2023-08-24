08fc89778d573590ce51770404d5dcfbb3731669
Matthew Koski <maccam912@gmail.com>
2023-08-24 18:02:11 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-cd llama.cpp && make main -j && cp main /usr/local/bin/ && cd ..
-if [ ! -f /app/models/nous-hermes-llama2-13b.ggmlv3.q8_0.bin ]; then
+cd llama.cpp && make -j && cp server /usr/local/bin/ && cd ..
+if [ ! -f /app/models/codellama-34b-instruct.Q5_K_M.gguf ]; then
     pushd /app/models
-    wget https://huggingface.co/TheBloke/Nous-Hermes-Llama2-GGML/resolve/main/nous-hermes-llama2-13b.ggmlv3.q8_0.bin
+    wget https://huggingface.co/TheBloke/CodeLlama-34B-Instruct-GGUF/resolve/main/codellama-34b-instruct.Q5_K_M.gguf
     popd
 fi
 
-uvicorn server:app --host=0.0.0.0 --log-level info
+server -m /app/models/codellama-34b-instruct.Q5_K_M.gguf
