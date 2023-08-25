0d5e68f58a2db8cb9008742c49e0856f7766fdeb
Matthew Koski <maccam912@gmail.com>
2023-08-24 20:09:32 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ set -ex
 cd llama.cpp && make -j && cp server /usr/local/bin/ && cd ..
 if [ ! -f /app/models/codellama-34b-instruct.Q8_0.gguf ]; then
     pushd /app/models
-    wget https://huggingface.co/TheBloke/CodeLlama-34B-Instruct-GGUF/blob/main/codellama-34b-instruct.Q8_0.gguf
+    wget https://huggingface.co/TheBloke/CodeLlama-34B-Instruct-GGUF/resolve/main/codellama-34b-instruct.Q8_0.gguf
 fi
 
 cp /app/models/codellama-34b-instruct.Q8_0.gguf /app/codellama-34b-instruct.Q8_0.gguf
