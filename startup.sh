8875743d088e9e5c9595dd61337b6a8796485f0c
Matthew Koski <maccam912@gmail.com>
2023-08-26 17:48:13 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 set -ex
 
 cd llama.cpp && make -j && cp server /usr/local/bin/ && cd ..
-if [ ! -f /app/models/phind-codellama-34b-v1.Q5_K_M.gguf ]; then
+if [ ! -f /app/models/phind-codellama-34b-instruct.Q5_K_M.gguf ]; then
     pushd /app/models
-    wget https://huggingface.co/TheBloke/Phind-CodeLlama-34B-v1-GGUF/resolve/main/phind-codellama-34b-v1.Q5_K_M.gguf
+    wget https://huggingface.co/TheBloke/Phind-CodeLlama-34B-instruct-GGUF/resolve/main/phind-codellama-34b-instruct.Q5_K_M.gguf
 fi
 
-cp /app/models/phind-codellama-34b-v1.Q5_K_M.gguf /app/phind-codellama-34b-v1.Q5_K_M.gguf
-server -m /app/phind-codellama-34b-v1.Q5_K_M.gguf --host "0.0.0.0"
+cp /app/models/phind-codellama-34b-instruct.Q5_K_M.gguf /app/phind-codellama-34b-instruct.Q5_K_M.gguf
+server -m /app/phind-codellama-34b-instruct.Q5_K_M.gguf --host "0.0.0.0"
