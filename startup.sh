2fef8b82f754c0f520522119859cf23a3ce0f783
Matthew Koski <maccam912@gmail.com>
2023-08-27 17:32:41 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -ex
 
+URL = "https://huggingface.co/TheBloke/WizardCoder-Python-13B-V1.0-GGUF/resolve/main/wizardcoder-python-13b-v1.0.Q8_0.gguf"
+MODEL = "wizardcoder-python-13b-v1.0.Q8_0.gguf"
+
 cd llama.cpp && make -j && cp server /usr/local/bin/ && cd ..
-if [ ! -f /app/models/codellama-34b-instruct.Q8_0.gguf ]; then
+if [ ! -f /app/models/$MODEL ]; then
     pushd /app/models
-    wget https://huggingface.co/TheBloke/CodeLlama-34B-Instruct-GGUF/resolve/main/codellama-34b-instruct.Q8_0.gguf
+    wget $URL
 fi
 
-cp /app/models/codellama-34b-instruct.Q8_0.gguf /app/codellama-34b-instruct.Q8_0.gguf
-server -m /app/codellama-34b-instruct.Q8_0.gguf --host "0.0.0.0"
+cp /app/models/$MODEL /app/$MODEL
+server -m /app/$MODEL --host "0.0.0.0"
