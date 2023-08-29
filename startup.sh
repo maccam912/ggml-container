210d7383a6a811e2a316615f9956f82b5b78aa87
Matthew Koski <maccam912@gmail.com>
2023-08-28 20:59:00 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ set -ex
 URL="https://huggingface.co/TheBloke/CodeLlama-34B-Instruct-GGUF/resolve/main/codellama-34b-instruct.Q5_K_M.gguf"
 MODEL="codellama-34b-instruct.Q5_K_M.gguf"
 
-cd llama.cpp && make -j && cp server /usr/local/bin/ && cd ..
+cd llama.cpp && make LLAMA_OPENBLAS=1 -j && cp server /usr/local/bin/ && cd ..
 if [ ! -f /app/models/$MODEL ]; then
     pushd /app/models
     wget $URL
