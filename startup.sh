4d7e7c31497bdfbc9e32c28d50e17a6354233cce
Matthew Koski <maccam912@gmail.com>
2024-02-13 19:18:27 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
-set -ex
+set -e
 
 # Use environment variables if they are set, otherwise use default values
 MODEL=${MODEL:-""}
 URL=${URL:-""}
 DEBUG=${DEBUG:-""}
+PARTS=${PARTS:-1} # Indicates the number of parts. Default is 1 for a single part.
 
-#cd llama.cpp && make LLAMA_OPENBLAS=1 -j && cp server /usr/local/bin/ && cd ..
 cd llama.cpp && make -j && cp server /usr/local/bin/ && cd ..
+
+download_and_verify() {
+    local url=$1
+    local target_path=$2
+
+    wget -O "$target_path" "$url" || { echo "Download failed, deleting partial file."; rm -f "$target_path"; exit 1; }
+
+    # Placeholder for actual verification logic, such as checksum verification
+}
 
 # Only download the model if URL is set
 if [ ! -z "$URL" ]; then
-    if [ ! -f /app/models/$MODEL ]; then
-        pushd /app/models
-        
-        # Only sleep if DEBUG is set
-        if [ ! -z "$DEBUG" ]; then
-            sleep 3600
-        fi
+    if [ "$PARTS" -eq 1 ]; then
+        local filename="/app/models/$MODEL"
+        if [ ! -f "$filename" ]; then
+            pushd /app/models
 
-        wget $URL
+            if [ ! -z "$DEBUG" ]; then
+                sleep 3600
+            fi
+
+            download_and_verify "$URL" "$filename"
+
+            popd
+        fi
+    else
+        for part in $(seq 1 $PARTS); do
+            local part_suffix=$(printf "\x61" | tr '\000-\177' '\141-\172' | cut -c $part) # Convert part number to a letter starting from 'a'
+            local filename="/app/models/${MODEL}-split-$part_suffix"
+            if [ ! -f "$filename" ]; then
+                pushd /app/models
+
+                if [ ! -z "$DEBUG" ]; then
+                    sleep 3600
+                fi
+
+                local part_url="${URL}-split-$part_suffix"
+                download_and_verify "$part_url" "$filename"
+
+                popd
+            fi
+        done
+
+        # Combine parts if necessary and clean up
+        pushd /app/models
+        cat ${MODEL}-split-* > "$MODEL"
+        rm ${MODEL}-split-*
         popd
     fi
 fi
 
-# cp /app/models/$MODEL /app/$MODEL
-server -m /app/models/$MODEL --host "0.0.0.0" --no-mmap -c 32768 -b 32768 -cb --numa #-spf /app/systemprompt.json
+# Start server with the (potentially reassembled) model
+server -m /app/models/$MODEL --host "0.0.0.0" --no-mmap -c 32768 -b 32768 -cb --numa
